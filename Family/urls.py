db9a61c22151f5e1f543e1ba61be044bfa943138
Alfonso Antonio Flores <alfonsoantonio234@gmail.com>
2022-08-23 12:25:12 -0500

    Fin

--- Family/urls.py
+++ Family/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from AppFamily.models import familia
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path
+    path("familia/",familia),
 ]
